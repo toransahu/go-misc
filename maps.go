58b3d5c27eb52857963b392111cfa1fe5f281703
Toran Sahu <toran.sahu@yahoo.com>
2019-06-17 00:18:14 +0530

    map

---
+++ maps.go
@@ -0,0 +1,26 @@
+//
+// maps.go
+// Copyright (C) 2019 Toran Sahu <toran.sahu@yahoo.com>
+//
+// Distributed under terms of the MIT license.
+//
+
+package main
+
+import "fmt"
+
+func main() {
+	asciiTable := make(map[string]int)
+	asciiTable["A"] = 65
+	asciiTable["a"] = 97
+	fmt.Println(asciiTable)
+
+	map1 := make(map[string]string)
+	map1["A"] = "65"
+	fmt.Println(map1)
+
+	map2 := map[string]string{"A": "65", "a": "97"}
+	fmt.Println(map2)
+
+	map3 := map[string][]string{"in": ["India", "Bharat"]}
+}
