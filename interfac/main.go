f9a7be07e1989796783d3b2997f4ff17e3be0ad9
Toran Sahu <toran.sahu@yahoo.com>
2019-07-17 13:19:08 +0530

    more examples

--- interfac/main.go
+++ interfac/main.go
@@ -9,8 +9,38 @@ package main
 
 import "fmt"
 
-// UseInterface explains usage of empty inteface{} & type assertions.
+// UseInterface explains creating a arbitrary struct without using any well defined struct - similar to python dict
+// or say creating map of string vs objects
 func UseInterface() {
+	i1 := map[string]interface{}{
+		"id":   1111,
+		"name": "Toran",
+		"interests": []interface{}{
+			"technology",
+			"trekking",
+			"biking",
+		},
+		"extra": map[string]interface{}{
+			"edu": "graduation",
+		},
+	}
+	fmt.Println(i1)
+
+	i2 := []interface{}{
+		map[string]interface{}{
+			"id":   1111,
+			"name": "Toran",
+		},
+		map[string]interface{}{
+			"id":   1112,
+			"name": "Abhishek",
+		},
+	}
+	fmt.Println(i2)
+}
+
+// UseInterface explains usage of empty inteface{} & type assertions.
+func UseInterfaceTypeAssertion() {
 	var i interface{}
 	i = "Some String"
 	fmt.Println(i, i.(string)) // type assertion
@@ -32,18 +62,22 @@ func InterfaceTypeAssertionInSwitch(i interface{}) {
 		fmt.Printf("%v is a float64\n", v)
 	case string:
 		fmt.Printf("%v is a string\n", v)
+	default:
+		fmt.Println("Couldn't detect")
 	}
+
 }
 
 func main() {
+	UseInterface()
+
 	var i interface{}
 	i = 1
 	InterfaceTypeAssertionInSwitch(i)
-
 	i = 1.1
 	InterfaceTypeAssertionInSwitch(i)
-
 	i = "Something"
 	InterfaceTypeAssertionInSwitch(i)
-	// UseInterface()
+
+	// UseInterfaceTypeAssertion()
 }
